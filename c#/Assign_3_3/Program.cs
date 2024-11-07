1df8aeb1546edea29faa84b5b9ec1ccda26058a9
Mohith Kashyap <mohithk@infinite.com>
2024-11-06 20:46:06 -0800

    assignment-3

---
+++ c#/Assign_3_3/Program.cs
@@ -0,0 +1,107 @@
+﻿
+namespace day3_assignments { 
+class Student
+{
+    public string Rollno;
+    public string Name;
+    public string Class;
+    public string Semester;
+    public string Branch;
+
+   
+    public Student(string roll_no, string name, string Std_class, string sem, string branch)
+    {
+        this.Rollno = roll_no;
+        this.Name = name;
+        this.Class = Std_class;
+        this.Semester = sem;
+        this.Branch = branch;
+    }
+    public void Studentdetails()
+    {
+        Console.WriteLine("========== Student Details ==========");
+        Console.WriteLine($"Name : {Name}");
+        Console.WriteLine($"Roll no : {Rollno}");
+        Console.WriteLine($"Class : {Class}");
+        Console.WriteLine($"Semester : {Semester}");
+        Console.WriteLine($"Branch : {Branch}");
+    }
+}
+
+class Marks : Student
+{
+    public Marks(string roll_no, string name, string Std_class, string sem, string branch) : base(roll_no, name, Std_class, sem, branch)
+    {
+
+    }
+    
+
+    public int[] marks = new int[5];
+    public void Getmarks()
+    {
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.Write("Enter Subject {0} marks : ", i);
+            marks[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+   
+    public void DisplayResult()
+    {
+        
+        int Total_marks = 0;
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Total_marks += marks[i];
+        }
+        Console.WriteLine("========= Total marks ===========");
+        Console.WriteLine("Total marks : { Total_marks} ");
+        Console.WriteLine(" ");
+        Console.WriteLine("======== Average percentage =======");
+        float average = (Total_marks / marks.Length);
+        Console.WriteLine("Average of all marks : { average}");
+
+
+
+        foreach (int i in marks)
+        {
+            if (i > 35 && average > 50) 
+                    Console.WriteLine("Pass");
+            else 
+                    Console.WriteLine("Fail");
+        }
+    }
+
+    public void DisplayData()
+    {
+        Studentdetails();
+        DisplayResult();
+    }
+
+}
+class Inheritance
+{
+    public static void Main(String[]args)
+    {
+        Console.Write("Enter Student name : ");
+        string Name = Console.ReadLine();
+        Console.Write("Enter Student Roll No : ");
+        string Rollno = Console.ReadLine();
+        Console.Write("Enter Student Class : ");
+        string Class = Console.ReadLine();
+        Console.Write("Enter Which Semester : ");
+        string sem = Console.ReadLine();
+        Console.Write("Enter Branch : ");
+        string branch = Console.ReadLine();
+        Marks mark = new Marks(Name, Rollno, Class, sem, branch);
+        Console.WriteLine(" ");
+        Console.WriteLine("*** Get marks ***");
+        mark.Getmarks();
+        Console.WriteLine(" ");
+        mark.DisplayData();
+        Console.ReadKey();
+    }
+}
+
+}
---
+++ c#/assign_sales/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Assignment_3
+{
+
+
+    class SalesDetails
+    {
+        public static int Sales_No;
+        public static int Product_No;
+        public static int Price;
+        public static DateTime DateofSale;
+        public static int Quantity;
+        public static int TotalAmount;
+
+
+        public SalesDetails(int sales_no, int product_no, int price, int qty, DateTime Dos)
+        {
+            Sales_No = sales_no;
+            Product_No = product_no;
+            Price = price;
+            Quantity = qty;
+            DateofSale = Dos;
+
+        }
+
+      
+        public void Sales(int price, int qty)
+        {
+            TotalAmount = qty * price;
+        }
+    }
+    class SalesDetails1 : SalesDetails
+    {
+        public SalesDetails1(int sales_no, int product_no, int price, int qty, DateTime date_of_sale)
+            : base(sales_no, product_no, price, qty, date_of_sale)
+        {
+
+        }
+
+        
+        public static void Show_Data()
+        {
+            Console.WriteLine("========== Total Bill ============");
+            Console.WriteLine($"Sales No: { Sales_No}");
+            Console.WriteLine($"Product No: {Product_No}");
+            Console.WriteLine($"Price : {Price}");
+            Console.WriteLine($"Quantity : {Quantity}");
+            Console.WriteLine($"Date of sale : {DateofSale}");
+            Console.WriteLine($"Total Amount : {TotalAmount}");
+        }
+        public static void Main()
+        {
+            Console.Write("Enter the Sales Number:");
+            int sale_no = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the Product Number:");
+            int product_no = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the Price:");
+            int price = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the Quantity:");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the Date of sale:");
+            DateTime dateofsale = Convert.ToDateTime(Console.ReadLine());
+            
+            SalesDetails Sdetails = new SalesDetails(sale_no, product_no, price, quantity, dateofsale);
+            Sdetails.Sales(price, quantity);
+            
+            Show_Data();
+           
+        }
+    }
+}
