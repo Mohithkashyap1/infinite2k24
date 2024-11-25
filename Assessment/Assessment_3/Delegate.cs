9e83cb26b117e32dd2983945fb02aabe21aed702
Mohith Kashyap <mohithk@infinite.com>
2024-11-24 20:07:15 -0800

    assign-1

---
+++ Assessment/Assessment_3/Delegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_3
+{
+    delegate int CalculatorDelegate(int num1, int num2);
+
+    class Delegate
+    {
+        public static int Add(int num1, int num2)
+        { return num1 + num2; }
+        public static int Subtract(int num1, int num2)
+        { return num1 - num2; }
+        public static int Multiply(int num1, int num2)
+        { return num1 * num2; }
+
+         public static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            CalculatorDelegate calcDelegate;
+
+            calcDelegate = Add;
+            Console.WriteLine($"Addition: {calcDelegate(num1, num2)}");
+
+            calcDelegate = Subtract;
+            Console.WriteLine($"Subtraction: {calcDelegate(num1, num2)}");
+
+            calcDelegate = Multiply;
+            Console.WriteLine($"Multiplication: {calcDelegate(num1, num2)}");
+        }
+    }
+}
---
+++ Assessment/Assessment_3/Writer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_3
+{
+        class Writer
+        {
+            public static void Main(String[] args)
+            {
+                string filePath = "C:\\dotnet training infinite\\Assessment\\Mohith.txt";
+                Console.Write("Enter the text to append: ");
+                string txt = Console.ReadLine();
+
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine(txt);
+                }
+
+                Console.WriteLine("Text appended successfully.");
+                Console.WriteLine("Appended text:");
+                Console.WriteLine(File.ReadAllText(filePath));
+
+                
+            }
+        }
+    }
+   
+
